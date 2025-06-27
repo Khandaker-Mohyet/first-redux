d1d8d52a155ff8bb1af0d29b32e211d802ce239c
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-27 19:04:37 +0600

    incriment and decriment doen

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
+import { useDispatch, useSelector } from "react-redux"
+import { decrement, increment } from "./redux/features/counter/counterSlice"
 
 function App() {
+  const dispatch = useDispatch();
+  const {count} = useSelector((state)=>state.counter);
+  
+
+  const handleIncrement=()=>{
+    dispatch(increment());
+  }
+  const handleDecrement=()=>{
+    dispatch(decrement());
+  }
 
 
   return (
     <>
     <h1>Counter with redux</h1>
-      <button>Increment</button>
-      <div>0</div>
-      <button>Decrement</button>
+      <button onClick={handleIncrement}>Increment</button>
+      <div>{count}</div>
+      <button onClick={handleDecrement}>Decrement</button>
     </>
   )
 }
